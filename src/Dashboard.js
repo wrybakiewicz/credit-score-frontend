e5db37acb23fd02727e3fcdde430d7cf8518cc3e
Jonasz Łaba <jonasz.laba@clebre.com>
2022-03-22 13:11:27 +0100

    Showing score

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -5,15 +5,31 @@ import {getCreditScore} from './api'
 
 export default function Dashboard() {
     const [creditScore, setCreditScore] = useState({})
+
     function fetchCreditScore(){
         getCreditScore('0x8A03E0daB7E83076Af7200B09780Af7856F0298D').then(res => {
             setCreditScore(res)
         })
     }
+    console.log(creditScore);
+    
+    function renderTokenHoldings(){
+        return creditScore.details.tokenHoldingDetails.details.map((token,index) =>{
+            return (
+                <tr>
+                    <td>{token.token}</td>
+                </tr>
+            )
+        })}
+    
     
     useEffect( () => {fetchCreditScore()}, []);
 
     return (
-        <p>{creditScore.score}</p>
+       <div>
+           <p>Score: {creditScore.score}</p>
+           <p>Token Holdings score: </p>
+           {renderTokenHoldings()}
+        </div>
     )
-}
+};
