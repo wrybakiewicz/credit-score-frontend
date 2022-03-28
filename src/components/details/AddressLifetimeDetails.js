78e75505b982f236511e006002b94114836c801d
Wojciech Rybakiewicz <wojtek.rybakiewicz@gmail.com>
2022-03-28 21:06:05 +0200

    fix spelling error

--- src/components/details/AddressLifetimeDetails.js
+++ src/components/details/AddressLifetimeDetails.js
@@ -4,7 +4,7 @@ export default function AddressLifetimeDetails({details}) {
     if (details.created && details.lifetimeInDays) {
         return <div>
             First transaction on address was executed on <b>{details.created}</b> which
-            is <b>{details.lifetimeInDays}</b> days ago
+            was <b>{details.lifetimeInDays}</b> days ago
         </div>;
     } else {
         return <div>Address does not have transactions</div>
