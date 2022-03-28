3afc848ff79ab1724226ff2f0a38ed23a97dba73
Wojciech Rybakiewicz <wojtek.rybakiewicz@gmail.com>
2022-03-28 19:46:46 +0200

    Add address lifetime score

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,11 +2,9 @@ import React, {useState} from 'react'
 import {getCreditScore} from '../api'
 import AddressForm from './AddressForm';
 import Account from "./Account";
-import ScoreTable from './ScoreTable';
 import {ethers} from "ethers";
 import Loader from "./Loader";
 import ScoreCard from './ScoreCards';
-import { flexbox } from '@mui/system';
 
 const provider = new ethers.providers.JsonRpcProvider(process.env.REACT_APP_JSON_PROVIDER_URL);
 
@@ -53,36 +51,55 @@ export default function Dashboard() {
                 throw resolveError;
             })
             .then(address => {
-            if(address === null) {
-                if(addressToVerify !== "") {
-                    setShowInvalidAddressError(true);
+                if (address === null) {
+                    if (addressToVerify !== "") {
+                        setShowInvalidAddressError(true);
+                    }
+                    throw Error("Invalid address/ENS");
                 }
-                throw Error("Invalid address/ENS");
-            }
-            return address;
-        });
+                return address;
+            });
     }
 
+    const showAddressLifetimeScore = (details) => {
+        if (details.created && details.lifetimeInDays) {
+            return <div>
+                First transaction on address was executed on <b>{details.created}</b> which
+                is <b>{details.lifetimeInDays}</b> days ago
+            </div>;
+        } else {
+            return <div>Address does not have transactions</div>
+        }
+    }
 
     return (
         <div>
             {<Account calculateScoreCallback={calculateScoreCallback}/>}
-            {<AddressForm onButtonSubmit={onSubmit} onInputChange={onInputChange} onBlur={() => verifyAddress(address)} address={address} showInvalidAddressError={showInvalidAddressError}/>}
+            {<AddressForm onButtonSubmit={onSubmit} onInputChange={onInputChange} onBlur={() => verifyAddress(address)}
+                          address={address} showInvalidAddressError={showInvalidAddressError}/>}
             {!showCalculating && creditScore.score !== undefined ?
                 <div>
-                    <div style={{ display:'flex', justifyContent:'center' }}>
+                    <div style={{display: 'flex', justifyContent: 'center'}}>
                         <h1>Your Score: {creditScore.score.toFixed(2)}</h1>
                     </div>
-                    <ScoreCard title = {"Token Holdings"} score = {creditScore.details.tokenHoldingDetails} details = {<p>asd</p>}/>
-                    <ScoreCard title = {"Aave"} score = {creditScore.details.aaveAddressDetails} details = {<p>asd</p>}/>
-                    <ScoreCard title = {"Social"} score = {creditScore.details.friendsSocialScore} details = {<p>asd</p>}/>
-                    <ScoreCard title = {"Twitter"} score = {creditScore.details.twitterDetails} details = {<p>asd</p>}/>
-                    <ScoreCard title = {"Cyber Connect"} score = {creditScore.details.cyberConnectDetails} details = {<p>asd</p>}/>
-                    <ScoreCard title = {"Address Lifetime"} score = {creditScore.details.addressCreation} details = {<p>asd</p>}/>
-                    </div>
+                    <ScoreCard title={"Token Holdings"} score={creditScore.details.tokenHoldingDetails}
+                               details={(score) => JSON.stringify(score.details)}/>
+                    <ScoreCard title={"Aave"} score={creditScore.details.aaveAddressDetails}
+                               details={(score) => JSON.stringify(score.details)}/>
+                    <ScoreCard title={"Social"} score={creditScore.details.friendsSocialScore}
+                               details={(score) => JSON.stringify(score.details)}/>
+                    <ScoreCard title={"Twitter"} score={creditScore.details.twitterDetails}
+                               details={(score) => JSON.stringify(score.details)}/>
+                    <ScoreCard title={"Cyber Connect"} score={creditScore.details.cyberConnectDetails}
+                               details={(score) => JSON.stringify(score.details)}/>
+                    <ScoreCard title={"Address Lifetime"} score={creditScore.details.addressCreation}
+                               details={(score) => showAddressLifetimeScore(score.details)}/>
+                    <ScoreCard title={"Poaps"} score={creditScore.details.poapsDetails}
+                               details={(score) => JSON.stringify(score.details)}/>
+                </div>
                 : null}
-            {showCalculating && !showInvalidAddressError  ?
-                <Loader />
+            {showCalculating && !showInvalidAddressError ?
+                <Loader/>
                 : null}
         </div>
     )
--- src/components/ScoreCards.js
+++ src/components/ScoreCards.js
@@ -1,61 +1,53 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
+import {styled} from '@mui/material/styles';
 import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
 import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
-    duration: theme.transitions.duration.shortest,
-  }),
+    const {expand, ...other} = props;
+    return <IconButton {...other} />;
+})(({theme, expand}) => ({
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+    }),
 }));
 
 export default function ScoreCard({title, score, details}) {
-  const [expanded, setExpanded] = React.useState(false);
+    const [expanded, setExpanded] = React.useState(false);
 
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
 
-  return (
-    <div style={{ display:'flex', justifyContent:'center' }}>
-    <Card sx={{width:1/2 , margin: 1 }}>   
-      <CardContent sx ={{display:'flex', justifyContent:'space-between', alignItems:'baseline'}}>
-        <Typography>{title + " score: " + score.score.toFixed(2)}</Typography>
-        <Typography> Details
-            <ExpandMore
-            expand={expanded}
-            onClick={handleExpandClick}
-            aria-expanded={expanded}
-            aria-label="show more"
-            >
-            <ExpandMoreIcon />
-            </ExpandMore>
-        </Typography>
-      </CardContent>
-    
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-            {JSON.stringify(score.details)}
-        </CardContent>
-      </Collapse>
-    </Card>
-    </div>
-  );
+    return (
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+            <Card sx={{width: 1 / 2, margin: 1}}>
+                <CardContent sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'baseline'}}>
+                    <Typography>{title + " score: " + score.score.toFixed(2)}</Typography>
+                    <Typography> Details
+                        <ExpandMore
+                            expand={expanded}
+                            onClick={handleExpandClick}
+                            aria-expanded={expanded}
+                            aria-label="show more"
+                        >
+                            <ExpandMoreIcon/>
+                        </ExpandMore>
+                    </Typography>
+                </CardContent>
+
+                <Collapse in={expanded} timeout="auto" unmountOnExit>
+                    <CardContent>
+                        {details(score)}
+                    </CardContent>
+                </Collapse>
+            </Card>
+        </div>
+    );
 }
