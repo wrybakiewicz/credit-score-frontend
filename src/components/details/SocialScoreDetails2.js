70abe7fb9b9c6f03ca5f0925470b0fe287f56578
Jonasz Łaba <jonasz.laba@clebre.com>
2022-03-30 08:01:23 +0200

    Spelling

--- src/components/details/SocialScoreDetails2.js
+++ src/components/details/SocialScoreDetails2.js
@@ -1,9 +1,12 @@
 import React from "react";
+import {Link, List, ListItem, ListItemText, ListSubheader} from "@mui/material";
 
-export default function SocialScoreDetails2({ details }) {
+export default function SocialScoreDetails2({ details,address }) {
     {
 
         return <div>
+            <p>This score is calculated based on address number of followers and number of accounts followed by address from <Link href={`https://app.cyberconnect.me/address/${address}`}
+                target={"_blank"}>Cyber Connect</Link></p>
             You have {details.followerCount} followers and you are following {details.followingCount} accounts.
         </div>;
     }
