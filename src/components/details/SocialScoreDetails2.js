9b0bc85138ab0befbdff881f3385635788c206d7
Jonasz Łaba <jonasz.laba@clebre.com>
2022-03-30 08:03:08 +0200

    formating

--- src/components/details/SocialScoreDetails2.js
+++ src/components/details/SocialScoreDetails2.js
@@ -7,7 +7,7 @@ export default function SocialScoreDetails2({ details,address }) {
         return <div>
             <p>This score is calculated based on address number of followers and number of accounts followed by address from <Link href={`https://app.cyberconnect.me/address/${address}`}
                 target={"_blank"}>Cyber Connect</Link></p>
-            You have {details.followerCount} followers and you are following {details.followingCount} accounts.
+            You have <b>{details.followerCount}</b> followers and you are following <b>{details.followingCount}</b> accounts.
         </div>;
     }
 }
